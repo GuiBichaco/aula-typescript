11141255f4132e3402a2f0d3176c1840f48a9549
Guilherme Catelli Bichaco <guicomercial328@gmail.com>
2025-02-24 09:16:44 -0300

    Adicionando os exercícios de Type Script

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 let mensagem: string = "Olá, TypeScript!";
 console.log(mensagem);
+
+// Exercício 1 - Tipagem e Arrays
+// Crie um array de números e imprima a soma dos valores.
+
+
+
+// Exercício 2 - Função Tipada
+// Crie uma função que recebe um nome e retorna uma mensagem personalizada.
+
+
+
+// Exercício 3 - Classe e Instância
+// Crie uma classe Carro com atributos marca, modelo e ano. Instancie um objeto e exiba suas informações.
