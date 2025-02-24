7671db37097ed1630834894b4e9254eb1a96d621
Guilherme Catelli Bichaco <guicomercial328@gmail.com>
2025-02-24 09:19:20 -0300

    Adicionando resolução do exercício 2

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,12 @@ let valores: number[] = [10, 20, 30];
 let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
 console.log("Soma dos valores:", somaValores);
 
-
 // Exercício 2 - Função Tipada
 // Crie uma função que recebe um nome e retorna uma mensagem personalizada.
-
+function mensagemPersonalizada(nome: string): string {
+    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+}
+console.log(mensagemPersonalizada("Ana"));
 
 
 // Exercício 3 - Classe e Instância
