b2218029747a26a17973fc6725886cc015360be0
Guilherme Catelli Bichaco <guicomercial328@gmail.com>
2025-02-24 09:18:01 -0300

    Adicionando resolução do exercício 1

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ console.log(mensagem);
 
 // Exercício 1 - Tipagem e Arrays
 // Crie um array de números e imprima a soma dos valores.
-
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
 
 
 // Exercício 2 - Função Tipada
